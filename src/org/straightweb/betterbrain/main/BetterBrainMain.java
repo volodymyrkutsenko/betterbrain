1448568e10bddd39ec5c4e1c28a970dfa2a5d5b5
Volodymyr Kutsenko <vkutsenko.git@gmail.com>
2014-04-13 23:25:46 +0300

    First actual commit with code (that makes not that much useful though)

---
+++ src/org/straightweb/betterbrain/main/BetterBrainMain.java
@@ -0,0 +1,10 @@
+package org.straightweb.betterbrain.main;
+
+public class BetterBrainMain {
+
+	public static void main(String[] args) {
+		System.out.println("This is a pre-pre-pre-pre-pre-pre-pre alpha version of BetterBrain");
+		System.out.println("The development starts soon");
+	}
+
+}
