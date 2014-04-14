918ed2585ff80b34ed438a81d8f524dad973a881
Volodymyr Kutsenko <vkutsenko.git@gmail.com>
2014-04-14 23:40:50 +0300

    [#2] Added jUnit 4 sorce folder. Running tests on each project build

---
+++ test/org/straightweb/betterbrain/main/BetterBrainSampleTest.java
@@ -0,0 +1,15 @@
+package org.straightweb.betterbrain.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BetterBrainSampleTest {
+
+	@Test
+	public void alwaysPassingTest() {
+		org.junit.Assert.assertNotNull(new Object());
+	}
+
+}
