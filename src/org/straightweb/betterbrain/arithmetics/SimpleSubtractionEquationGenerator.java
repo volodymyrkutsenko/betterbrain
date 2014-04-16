1219f5d474cba6182daaba330485a8560ddd9266
Volodymyr Kutsenko <vkutsenko.git@gmail.com>
2014-04-17 02:18:12 +0300

    [#5] Adjusted subtraction equations generator to generate only equations with result < 10

--- src/org/straightweb/betterbrain/arithmetics/SimpleSubtractionEquationGenerator.java
+++ src/org/straightweb/betterbrain/arithmetics/SimpleSubtractionEquationGenerator.java
@@ -2,8 +2,8 @@ package org.straightweb.betterbrain.arithmetics;
 
 public class SimpleSubtractionEquationGenerator extends SimpleArithmeticEquationGenerator {
 
-	private static final int FIRST_ARGUMENT_UPPER_LIMIT = 19;
-	private static final int SECOND_ARGUMENT_UPPER_LIMIT = 10;
+	private static final int FIRST_ARGUMENT_UPPER_LIMIT = 18;
+	private static final int SECOND_ARGUMENT_UPPER_LIMIT = 9;
 	
 	@Override
 	public SimpleArithmeticEquation generateEquation() {
@@ -13,14 +13,16 @@ public class SimpleSubtractionEquationGenerator extends SimpleArithmeticEquation
 	}
 	
 	private int generateFirstArgument() {
-		return getRandom().nextInt(FIRST_ARGUMENT_UPPER_LIMIT);
+		return getRandom().nextInt(FIRST_ARGUMENT_UPPER_LIMIT + 1);
 	}
 	
 	private int generateSecondArgument(int firstArgument) {
-		int secondArgument;
-		do {
-			secondArgument = getRandom().nextInt(SECOND_ARGUMENT_UPPER_LIMIT);
-		} while (secondArgument > firstArgument);
+		int secondArgumentLowerLimit = firstArgument - SECOND_ARGUMENT_UPPER_LIMIT;
+		if (secondArgumentLowerLimit < 0) {
+			secondArgumentLowerLimit = 0;
+		}
+		int limitByFirstArgument = firstArgument + 1;
+		int secondArgument = getRandom().nextInt(limitByFirstArgument - secondArgumentLowerLimit < SECOND_ARGUMENT_UPPER_LIMIT + 1 - secondArgumentLowerLimit ? limitByFirstArgument - secondArgumentLowerLimit : SECOND_ARGUMENT_UPPER_LIMIT + 1 - secondArgumentLowerLimit) + secondArgumentLowerLimit;
 		return secondArgument;
 	}
 
--- test/org/straightweb/betterbrain/arithmetics/SimpleSubtractionEquationGeneratorTest.java
+++ test/org/straightweb/betterbrain/arithmetics/SimpleSubtractionEquationGeneratorTest.java
@@ -7,11 +7,12 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SimpleSubtractionEquationGeneratorTest {
 
-	private static final int TEST_SAMPLES_QUANTITY = 100;
+	private static final int TEST_SAMPLES_QUANTITY = 200;
 	
 	private static final int FIRST_ARGUMENT_UPPER_LIMIT = 19;
 	private static final int SECOND_ARGUMENT_UPPER_LIMIT = 10;
 	
+	private static final int RESULT_UPPER_LIMIT = 10;
 	private static final int RESULT_LOWER_LIMIT = 0;
 	
 	@Test
@@ -22,6 +23,16 @@ public class SimpleSubtractionEquationGeneratorTest {
 			org.junit.Assert.assertNotNull(additionEquation);
 			org.junit.Assert.assertTrue(additionEquation.getFirstArgument() < FIRST_ARGUMENT_UPPER_LIMIT);
 			org.junit.Assert.assertTrue(additionEquation.getSecondArgument() < SECOND_ARGUMENT_UPPER_LIMIT);
+		}
+	}
+	
+	@Test
+	public void testResultsUpperLimit() {
+		SimpleArithmeticEquationGenerator subtractionEquationGenerator = new SimpleSubtractionEquationGenerator();
+		for (int i = 0; i < TEST_SAMPLES_QUANTITY; i++) {
+			SimpleArithmeticEquation additionEquation = subtractionEquationGenerator.generateEquation();
+			org.junit.Assert.assertNotNull(additionEquation);
+			org.junit.Assert.assertTrue(additionEquation.getResult() < RESULT_UPPER_LIMIT);
 		}
 	}
 	
